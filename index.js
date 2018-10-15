e63b1afa65b7a207e5933ed4edaa898c094bd1bd
wippyzulkarnain <wippy.zulkarnain@gmail.com>
2018-10-15 16:58:38 +0700

    Level 1

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 let a=3
-let b=-30
-let c=40.27
+let b=7
+let c=12
 
 // Level 0
 console.log("Level 0")
 console.log("a = ",a)
 console.log("b = ",b)
 console.log("c = ",c)
+
+// Level 1
+console.log("Level 1")
+console.log("a+b+c = ",a+b+c)
+console.log("a-b-c = ",a-b-c)
+console.log("a*b*c = ",a*b*c)
+console.log("a/b = ",a/b)
