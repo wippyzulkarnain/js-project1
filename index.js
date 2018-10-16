ce9ce9f07ce5462d110749d9dd2768e5d6499238
wippyzulkarnain <wippy.zulkarnain@gmail.com>
2018-10-16 09:01:05 +0700

    Level 3

--- index.js
+++ index.js
@@ -28,3 +28,21 @@ console.log("e = ",e)
 console.log("f = ",f)
 console.log("g = ",g)
 console.log("h = ",h)
+
+// Level 3
+let arr1 = [1,2,3]
+let arr2 = [4,5,6]
+let arr_combined = arr1.concat(arr2)
+console.log("array 1 = ",arr1)
+console.log("array 2 = ",arr2)
+console.log("array 1 + array 2 = ",arr_combined)
+arr_combined.push(7)
+console.log("menambahkan angka ke dalam array ",arr_combined)
+arr_combined.pop(7)
+console.log("menghilangkan angka dari array ",arr_combined)
+let i =1
+for (i=1 ; i <5 ; i++){
+    let x=arr_combined.length-1
+    arr_combined.push(arr_combined[x]+1)
+}
+console.log("menanmbahkan data ke dalam array dengan looping : ",arr_combined)
